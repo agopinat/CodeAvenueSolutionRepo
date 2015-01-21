7d2c13062b74deb34131666a079ae972fb5c73fb
Aravind Gopinath <aravindgopinath88@gmail.com>
2015-01-20 23:37:50 -0800

    Initial Commit

---
+++ source/CodeAvenueSolution/Scripts/FindTotal.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () { 
+     calculateTotal();
+});
+
+function calculateTotal() {
+    var total = 0;
+    //reference the rows you want to add
+    //this will not include the header row
+    var rows = $("#Cart tr:gt(0)");
+
+    rows.children("td:nth-child(6)").each(function () {
+        //each time we add the cell to the total
+        total += parseInt($(this).html());
+    });
+
+    //then output them to the elements
+    $("#total").html(total.toFixed(2));
+}
---
+++ source/CodeAvenueSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeAvenueSolution.Startup))]
+namespace CodeAvenueSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
